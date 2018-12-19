cf0fdfeb797320ae561def685cb6484efd857df5
Simon Li <spli@dundee.ac.uk>
2018-12-19 10:17:56 +0000

    Use omero.cli.cli_login for login. Use asFileObj. Flake8 fixes

--- scripts/rois-from-attachments.py
+++ scripts/rois-from-attachments.py
@@ -1,103 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-from os import (
-    SEEK_CUR,
-    SEEK_END,
-    SEEK_SET,
-)
-
-class OriginalFileLike(object):
-    """
-    Based on
-    https://github.com/openmicroscopy/openmicroscopy/blob/v5.4.7/components/tools/OmeroPy/src/omero/gateway/__init__.py#L5232
-    https://docs.python.org/2/library/stdtypes.html#file-objects
-    """
-    def __init__(self, originalfile, bufsize=2621440):
-        self.originalfile = originalfile
-        self.bufsize = bufsize
-        self.rfs = self.originalfile._conn.createRawFileStore()
-        self.rfs.setFileId(self.originalfile.id)
-        self.pos = 0
-
-    def __iter__(self):
-        return self.originalfile.getFileInChunks(self.bufsize)
-
-    def seek(self, n, mode=0):
-        if mode == SEEK_SET:
-            self.pos = n
-        elif mode == SEEK_CUR:
-            self.pos += n
-        elif mode == SEEK_END:
-            self.pos = self.rfs.size() + n
-        else:
-            raise ValueError('Invalid mode: %s' % mode)
-
-    def tell(self):
-        return self.pos
-
-    def read(self, n=-1):
-        buf = ''
-        if n < 0:
-            endpos = self.rfs.size()
-        else:
-            endpos = min(self.pos + n, self.rfs.size())
-        while self.pos < endpos:
-            nread = min(self.bufsize, endpos - self.pos)
-            buf += self.rfs.read(self.pos, nread)
-            self.pos += nread
-        return buf
-
-    def close(self):
-        self.rfs.close()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.close()
-
-
-# In[2]:
-
-
 import omero.clients
-from omero.gateway import BlitzGateway
+import omero.cli
 from omero.rtypes import rdouble, rint
-from getpass import getpass
 from scipy.io import loadmat
-#import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import re
-
-pd.set_option('max_rows', 5)
-
-
-# In[3]:
-
-
-username = raw_input('username: ')
-password = getpass()
-
-
-def connect(username, password):
-    conn = BlitzGateway(
-        username,
-        password,
-        host='localhost',
-        port=4064,
-        secure=True)
-    connected = conn.connect()
-    assert connected
-    conn.c.enableKeepAlive(60)
-    return conn
-
-
-# In[4]:
 
 
 def get_label_image(im):
@@ -112,7 +21,7 @@ def get_label_image(im):
             continue
     if not filename:
         raise Exception('Failed to get label image for %s' % im.name)
-    with OriginalFileLike(ann.getFile()) as f:
+    with ann.getFile().asFileObj() as f:
         mat = loadmat(f)
         return mat['cells']
 
@@ -126,13 +35,13 @@ def roi_from_binary_image(binim, rgba):
     y0 = min(ymask)
     h = max(ymask) - y0 + 1
     submask = binim[y0:(y0 + h), x0:(x0 + w)]
-    #print (x0, y0, w, h)
+    # print (x0, y0, w, h)
 
     mask = omero.model.MaskI()
     # BUG in older versions of Numpy:
     # https://github.com/numpy/numpy/issues/5377
     # Need to convert to an int array
-    #mask.setBytes(np.packbits(submask))
+    # mask.setBytes(np.packbits(submask))
     mask.setBytes(np.packbits(np.asarray(submask, dtype=int)))
     mask.setWidth(rdouble(w))
     mask.setHeight(rdouble(h))
@@ -146,6 +55,7 @@ def roi_from_binary_image(binim, rgba):
     roi.addShape(mask)
     return roi
 
+
 def create_rois(im):
     labels = get_label_image(im)
     print(im.name, labels.shape, labels.min(), labels.max())
@@ -156,6 +66,7 @@ def create_rois(im):
         rois.append(roi)
     return rois
 
+
 def save_rois(conn, im, rois):
     print('Saving %d ROIs for image %d:%s' % (len(rois), im.id, im.name))
     us = conn.getUpdateService()
@@ -164,36 +75,39 @@ def save_rois(conn, im, rois):
         im = conn.getObject('Image', im.id)
         roi.setImage(im._obj)
         roi1 = us.saveAndReturnObject(roi)
+        assert roi1
 
 
-# In[5]:
-
-
-conn = connect(username, password)
-print('Connected')
-project = conn.getObject('Project', attributes={'name': 'idr0047-neuert-yeastmRNA'})
-#images = [conn.getObject('Image', 4010034)]
-missing = []
-for dataset in project.listChildren():
-    if dataset.name == 'processed':
-        continue
-    images = list(dataset.listChildren())
-    print(len(images))
-    for im in images:
-        if im.name.startswith('M_') or im.name.startswith('SD_') or '.companion.ome' in im.name:
+def main(conn):
+    project = conn.getObject(
+        'Project', attributes={'name': 'idr0047-neuert-yeastmRNA'})
+    missing = []
+    for dataset in project.listChildren():
+        if dataset.name == 'processed':
             continue
-        try:
-            rois = create_rois(im)
-            save_rois(conn, im, rois)
-        except Exception as e:
-            print e
-            missing.append((im.id, im.name))
-            # raise
+        images = list(dataset.listChildren())
+        print(len(images))
+    # For testing with two images:
+    # `omero delete Image/Roi:4010034,4010056 --report`
+    # images = conn.getObjects('Image', [4010034, 4010056])
+        for im in images:
+            if (im.name.startswith('M_') or im.name.startswith('SD_') or
+                    '.companion.ome' in im.name):
+                continue
+            try:
+                rois = create_rois(im)
+                save_rois(conn, im, rois)
+            except Exception as e:
+                print e
+                missing.append((im.id, im.name))
+                # raise
 
-if missing:
-    print 'No segmentations found for:\n%s' % '\n'.join(m[1] for m in missing)
+    if missing:
+        print('No segmentations found for:\n%s' %
+              '\n'.join(m[1] for m in missing))
 
 
-# In[ ]:
-
-conn.close()
+if __name__ == '__main__':
+    with omero.cli.cli_login() as c:
+        conn = omero.gateway.BlitzGateway(client_obj=c.get_client())
+        main(conn)
